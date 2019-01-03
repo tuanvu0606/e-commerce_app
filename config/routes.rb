8c3feae01b0df74f5462e82b0f18f3de9e6de3be
tuan_vu <tuanvu0695@gmail.com>
2019-01-03 15:44:44 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about_us'
+  get 'static_pages/contact'
   resources :user_sessions
   resources :user_accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'user_sessions#index'
+  root 'static_pages#home'
 end
